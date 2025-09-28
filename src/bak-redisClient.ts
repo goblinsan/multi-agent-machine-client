1ae76ce33bedec9e02d505f33d499043b7907782
jimmothy co <coghlanjames@yahoo.com>
2025-09-28 08:42:57 -0400

    Remove bak-* files from repository

--- src/bak-redisClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-import { cfg } from "./config.js";
-
-export async function makeRedis() {
-  const client = createClient({ url: cfg.redisUrl, password: cfg.redisPassword });
-  client.on("error", (e) => console.error("[redis] error", e));
-  await client.connect();
-  return client;
-}
